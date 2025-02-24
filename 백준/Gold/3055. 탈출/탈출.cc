a3b591392ffb50fa738fa9ef764e481ed3a50b0c
SongMinQQ <koe7324@naver.com>
2025-02-24 11:26:23 +0900

    [Gold IV] Title: 탈출, Time: 0 ms, Memory: 2048 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.cc
+++ 백준/Gold/3055. 탈출/탈출.cc
@@ -9,53 +9,34 @@ queue<pair<int,int>> q2;
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
 
-void print(){
-  cout << '\n';
-  for(int i = 0; i < r; i++){
-    for(int j = 0; j < c; j++){
-      cout << visited2[i][j];
-    }
-    cout << '\n';
-  }
-  cout << '\n';
-}
 void fillWater(int y, int x){
+  visited2[y][x] = 1;
+  while(!q2.empty()){
+    tie(y,x) = q2.front();
+    q2.pop();
+    for(int i = 0; i < 4; i++){
+      int ny = y + dy[i];
+      int nx = x + dx[i];
+      if(ny < 0 || nx < 0 || ny >= r || nx >= c || visited2[ny][nx] || forest[ny][nx] == 'X' || forest[ny][nx] == 'S' || forest[ny][nx] == 'D')continue;
+      q2.push({ny,nx});
+      visited2[ny][nx] = visited2[y][x] + 1;
+    }
+  }
+}
+void move(int y, int x){
   visited[y][x] = 1;
-  // visited2[y][x] = 1;
   while(!q1.empty()){
-    int size = q2.size();
-    for(int q = 0; q < size; q++){
-      tie(y,x) = q2.front();
-      q2.pop();
-      for(int i = 0; i < 4; i++){
-        int ny = y + dy[i];
-        int nx = x + dx[i];
-        if(ny < 0 || nx < 0 || ny >= r || nx >= c || visited2[ny][nx] || forest[ny][nx] == 'X' || forest[ny][nx] == 'S' || forest[ny][nx] == 'D')continue;
-        if(!visited2[ny][nx]){
-          q2.push({ny,nx});
-          forest[ny][nx] = '*';
-          visited2[ny][nx] = visited2[y][x] + 1;
-        }
-      }
+    tie(y,x) = q1.front();
+    q1.pop();
+    if(visited2[y][x] == visited[y][x]) continue;
+    for(int i = 0; i < 4; i++){
+      int ny = y + dy[i];
+      int nx = x + dx[i];
+      if(ny < 0 || nx < 0 || ny >= r || nx >= c || visited[ny][nx] || forest[ny][nx] == 'X')continue;
+      // if(visited2[ny][nx] == visited[ny][nx] + 1) continue;
+      q1.push({ny,nx});
+      visited[ny][nx] = visited[y][x] + 1;
     }
-  // }
-  
-  // while(!q1.empty()){
-    int size2 = q1.size();
-    for(int q = 0; q < size2; q++){
-      tie(y,x) = q1.front();
-      q1.pop();
-      for(int i = 0; i < 4; i++){
-        int ny = y + dy[i];
-        int nx = x + dx[i];
-        if(ny < 0 || nx < 0 || ny >= r || nx >= c || visited[ny][nx] || forest[ny][nx] == 'X' || forest[ny][nx] == '*')continue;
-        // if(visited2[ny][nx] == visited[ny][nx] + 1) continue;
-        q1.push({ny,nx});
-        visited[ny][nx] = visited[y][x] + 1;
-        if(visited2[ny][nx] > visited[ny][nx]) continue;
-      }
-    }
-  // }
   }
 }
 int main(){
@@ -63,22 +44,21 @@ int main(){
   for(int i = 0 ; i < r; i++){
     for(int j = 0; j < c; j++){
       cin >> forest[i][j];
-      if(forest[i][j] == 'S'){
-        q1.push({i, j});
-      }
-      if(forest[i][j] == '*'){
-        visited2[i][j] = 1;
-        q2.push({i, j});
-      }
       if(forest[i][j] == 'D'){
         destinationY = i;
         destinationX = j;
       }
+      if(forest[i][j] == '*'){
+        q2.push({i, j});
+      }
+      if(forest[i][j] == 'S'){
+        q1.push({i, j});
+      }
     }
   }
-  fillWater(q1.front().first, q1.front().second);
-  // print();
+  fillWater(q2.front().first,q2.front().second);
+  move(q1.front().first, q1.front().second);
   if(visited[destinationY][destinationX] == 0) cout << "KAKTUS" << '\n';
-  else cout << visited[destinationY][destinationX] - 1 << '\n';
+  else cout << visited[destinationY][destinationX] -1 << '\n';
   return 0;
 }
