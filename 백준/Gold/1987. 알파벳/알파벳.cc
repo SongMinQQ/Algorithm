4c603694441e70aca7ea8d24e6dd3c344630b813
SongMinQQ <koe7324@naver.com>
2025-02-24 15:47:34 +0900

    [Gold IV] Title: 알파벳, Time: 16 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,32 +1,36 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
+int visited[21][21], r, c, result, board[21][21];
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
-char a[21][21];
-int r, c, result = 0, check;
+//메모이제이션으로 풀어보자
 
-void dfs(int y, int x, int num, int cnt){
-    result = max(result, cnt);
-    for(int i = 0; i < 4; i++){
-        int ny = y + dy[i];
-        int nx = x + dx[i];
-        if(ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
-        int nextNum = (1 << (int)(a[ny][nx] - 'A'));
-        if((num & nextNum) == 0) {
-            dfs(ny, nx, (num | nextNum), cnt + 1);
-        }
-    }
+void dfs(int y, int x, int cnt, int num){
+  result = max(result, cnt);
+  //이미 방문한 곳이면 그냥 끝냄
+  if(visited[y][x] == num) return;
+  visited[y][x] = num;
+  for(int i = 0; i < 4; i++){
+    int ny = y + dy[i];
+    int nx = x + dx[i];
+    if(ny < 0 || nx < 0 || ny >= r || nx >= c) continue;
+    //다음에 방문할 곳이 이미 방문한적이 있다면 넘김
+    if(num & (1 << board[ny][nx])) continue;
+    //근데 |= 과 | 의 차이는?
+    dfs(ny, nx, cnt + 1, num | (1 << board[ny][nx]));
+  }
 }
 int main(){
-    cin >> r >> c;
-    for(int i = 0; i < r; i++){
-        string line;
-        cin >> line;
-        for(int j = 0; j < line.size(); j++){
-            a[i][j] = line[j];
-        }
+  cin >> r >> c;
+  for(int i = 0; i < r; i++){
+    string line;
+    cin >> line;
+    for(int j = 0; j < c; j++){
+      board[i][j] = (int)line[j] - 'A';
     }
-    dfs(0, 0, (1 << (int)a[0][0] - 'A'), 1);
-    cout << result << '\n';
+  }
+  dfs(0, 0, 1, (1 << board[0][0]));
+  cout << result << '\n';
+  return 0;
 }
