358761484443435fa0d5ffe44f2e60e62715fa83
SongMinQQ <koe7324@naver.com>
2025-02-28 17:26:12 +0900

    [Gold V] Title: 강의실 배정, Time: 184 ms, Memory: 6128 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.cc
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n, s, t, result;
+vector<pair<int, int>> v;
+set<int> se;
+int main(){
+  cin >> n;
+  for(int i = 0; i < n; i++){
+    cin >> s >> t;
+    v.push_back({s, t});
+  }
+  sort(v.begin(),v.end());
+  priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+  for(pair<int, int> i : v){
+    // if(pq.size()) cout << pq.top().first << ' ' << pq.top().second << '\n';
+    if(pq.empty() || pq.top().first > i.first){
+      pq.push({i.second, i.first});
+      result++;
+    }
+    else if(pq.top().first <= i.first){
+      pq.push({i.second, i.first});
+      pq.pop();
+    }
+    else{
+      pq.pop();
+      pq.push({i.second, i.first});
+      result--;
+    }
+  }
+  cout << result << '\n';
+  return 0;
+}
