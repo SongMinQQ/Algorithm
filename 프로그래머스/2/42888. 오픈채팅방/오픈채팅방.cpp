5ecc0cd1d3c65a8f0134dd870ad357d278318b01
SongMinQQ <koe7324@naver.com>
2025-03-11 19:16:04 +0900

    [level 2] Title: 오픈채팅방, Time: 268.87 ms, Memory: 43.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+vector<string> split(string input, string delimiter){
+    vector<string> ret;
+    long long pos = 0;
+    string token = "";
+    while((pos = input.find(delimiter)) != string::npos){
+        token = input.substr(0,pos);
+        ret.push_back(token);
+        input.erase(0,pos + delimiter.length());
+    }
+    ret.push_back(input);
+    return ret;
+}
+vector<string> solution(vector<string> record) {
+    vector<string> answer;
+    map<string, string> m;
+    for(string i : record){
+        vector<string> sliceString = split(i, " ");
+        if(sliceString[0] == "Enter"){
+            if (m.find(sliceString[1]) != m.end()) {
+                m.insert({sliceString[1], sliceString[2]});
+            }
+            if(m[sliceString[1]] != sliceString[2])m[sliceString[1]] = sliceString[2];
+        }
+        else if(sliceString[0] == "Change"){
+            m[sliceString[1]] = sliceString[2];
+        }
+    }
+    for(string i : record){
+        string result;
+        vector<string> sliceString = split(i, " ");
+        if(sliceString[0] == "Enter"){
+            result = m[sliceString[1]] + "님이 들어왔습니다.";
+        }
+        else if(sliceString[0] == "Leave"){
+            result = m[sliceString[1]] + "님이 나갔습니다.";
+        }
+        else continue;
+        answer.push_back(result);
+    }
+    return answer;
+}
