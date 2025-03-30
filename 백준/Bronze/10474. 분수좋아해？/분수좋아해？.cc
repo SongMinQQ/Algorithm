409d5d277c599617a681bba66afc1d6fa03c5f56
SongMinQQ <koe7324@naver.com>
2025-03-30 21:41:52 +0900

    [Bronze III] Title: 분수좋아해?, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10474. 분수좋아해？/분수좋아해？.cc
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a, b;
+int main(){
+  while(1){
+    cin >> a >> b;
+    if(a == 0 && b == 0) return 0;
+    cout << a / b << ' ' << a % b << " / " << b << '\n';
+  }
+}
