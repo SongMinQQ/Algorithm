7c7ad008d44ddb3162e67c1e8d161c45624886f4
SongMinQQ <koe7324@naver.com>
2025-04-20 23:15:14 +0900

    [Silver IV] Title: 비트가 넘쳐흘러, Time: 40 ms, Memory: 4916 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17419. 비트가 넘쳐흘러/비트가 넘쳐흘러.cc
+++ 백준/Silver/17419. 비트가 넘쳐흘러/비트가 넘쳐흘러.cc
@@ -6,10 +6,14 @@ long long num;
 string line;
 int main(){
   cin >> n >> line;
-  num = stoll(line,nullptr,2);
-  while(num != 0){
-    num &= (num - 1);
-    ret++;
+  // num = stoll(line,nullptr,2);
+  // while(num != 0){
+  //   num &= (num - 1);
+  //   ret++;
+  // }
+  for(char i : line){
+    if(i == '1')
+      ret++;
   }
   cout << ret << '\n';
   return 0;
