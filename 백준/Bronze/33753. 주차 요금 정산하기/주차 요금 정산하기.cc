f1de86a96b0fcd89ce2b398e5f64952ebca01968
SongMinQQ <koe7324@naver.com>
2025-05-11 02:03:02 +0900

    [Bronze III] Title: 주차 요금 정산하기, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/33753. 주차 요금 정산하기/주차 요금 정산하기.cc
+++ 백준/Bronze/33753. 주차 요금 정산하기/주차 요금 정산하기.cc
@@ -5,9 +5,15 @@ int a, b, c;
 float t, de;
 int main(){
   cin >> a >> b >> c >> t;
-  if(t - 30 > 0){
-    de = floor(t / 30.0);
+  
+  if (t <= 30) {
+    cout << a << '\n';
+  } else {
+    int et = t - 30;
+    int efu = ceil(et / (double)b);
+    int result = a + efu * c;
+    cout << result << '\n';
   }
-  c <= 30 ? cout << a : cout << a + (c * de);
+
   return 0;
 }
