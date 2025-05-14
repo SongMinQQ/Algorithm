ff05469418da9e81a3a0d2ca8bcd2f7d0af88dcd
SongMinQQ <koe7324@naver.com>
2025-05-14 18:56:59 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 120 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.cc
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<pair<int, int>> v;
+int n, a, b, result;
+int main(){
+  cin >> n;
+  for (int i = 0; i < n; i++){
+    cin >> a >> b;
+    v.push_back({a, b});
+  }
+  sort(v.begin(), v.end());
+  priority_queue<int, vector<int>, greater<int>> pq;
+  for(pair<int,int> i : v){
+    if(!pq.empty() && i.first >= pq.top()){
+      pq.pop();
+      pq.push(i.second);
+    }
+    else pq.push(i.second);
+    result = max(result, (int)pq.size());
+  }
+  cout << result;
+  return 0;
+}
