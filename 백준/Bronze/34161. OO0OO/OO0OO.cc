1c1df245641880b879c5f6815e3517c545079975
SongMinQQ <koe7324@naver.com>
2025-09-11 18:14:57 +0900

    [Bronze IV] Title: OO0OO, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/34161. OO0OO/OO0OO.cc
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+string a;
+int main(){
+    int b = 3;
+    while(b--){
+        cin >> a;
+    }
+    for(int i = 0; i < 10000; i++){
+        cout << -1 << '\n';
+    }
+    return 0;
+}
