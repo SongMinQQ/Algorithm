9ad011812357eee52578c47c7610ad96148b87c2
SongMinQQ <koe7324@naver.com>
2025-12-20 23:12:57 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n, k, a, b;
+int main(){
+    cin >> n >> k;
+    while(n--){
+        cin >> a >> b;
+    }
+    cout << "비와이";
+    return 0;
+}
